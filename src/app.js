6580bf9435109b05177025509fe75ff7c3df795e
DESKTOP-F47U60N\gabri <gabrielahercules94@gmail.com>
2025-04-26 17:32:27 -0600

    Creando un proyecto de React, React Dom

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function app(){
+
+    return(
+<div>
+    <h1>Hola Mundo desde react</h1>
+    <p>Creando mi primera aplicacion desde React </p>
+</div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+
+
+const root = reactDom.createRoot(document.getElementById('root'));
+root.render(<app/>);
